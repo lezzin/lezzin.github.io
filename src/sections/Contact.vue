ce79ec58ef247e662deba171d2b1c8bd4b4c4e4b
Leandro Adrian da Silva <leandrinsilva22@gmail.com>
2024-11-06 19:26:15 -0300

    Add more UX

--- src/sections/Contact.vue
+++ src/sections/Contact.vue
@@ -9,7 +9,11 @@ const name = ref("");
 const email = ref("");
 const message = ref("");
 const isSending = ref(false);
-const errors = reactive({ name: "", email: "", message: "" });
+const errors = reactive({
+    name: "",
+    email: "",
+    message: ""
+});
 
 watch(name, () => errors.name = "");
 watch(email, () => errors.email = "");
@@ -112,22 +116,46 @@ onMounted(() => {
             <form class="delay-medium" @submit.prevent="sendMessage">
                 <div :class="{ 'form-error': errors.name }">
                     <input type="text" aria-label="Nome" name="nome" placeholder="Nome" v-model="name">
-                    <p class="form-error-message" v-if="errors.name">{{ errors.name }}</p>
+                    <p class="form-error-message" v-if="errors.name">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor"
+                            class="bi bi-exclamation-circle" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                            <path
+                                d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z" />
+                        </svg>
+                        {{ errors.name }}
+                    </p>
                 </div>
 
                 <div :class="{ 'form-error': errors.email }">
                     <input type="email" aria-label="Email" name="email" placeholder="Email" v-model="email">
-                    <p class="form-error-message" v-if="errors.email">{{ errors.email }}</p>
+                    <p class="form-error-message" v-if="errors.email">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor"
+                            class="bi bi-exclamation-circle" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                            <path
+                                d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z" />
+                        </svg>
+                        {{ errors.email }}
+                    </p>
                 </div>
 
                 <div :class="{ 'form-error': errors.message }">
                     <textarea aria-label="Mensagem" name="mensagem" rows="5" placeholder="Digite aqui a sua mensagem"
                         v-model="message"></textarea>
-                    <p class="form-error-message" v-if="errors.message">{{ errors.message }}</p>
+                    <p class="form-error-message" v-if="errors.message">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor"
+                            class="bi bi-exclamation-circle" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                            <path
+                                d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z" />
+                        </svg>
+                        {{ errors.message }}
+                    </p>
                 </div>
 
                 <button type="submit" class="btn success" :disabled="isSending">
-                    {{ isSending ? 'Enviando...' : 'Enviar mensagem' }}
+                    {{ isSending ? 'Enviando...' : '🚀 Enviar mensagem' }}
                 </button>
             </form>
         </div>
@@ -156,6 +184,8 @@ textarea {
 button {
     padding: 1rem 0;
 
+    &:hover {}
+
     &:disabled {
         cursor: not-allowed;
         opacity: .6;
@@ -174,5 +204,9 @@ button {
     color: var(--error-color);
     font-size: 1.4rem;
     margin-top: 0.25rem;
+
+    svg {
+        transform: translateY(1px);
+    }
 }
 </style>
