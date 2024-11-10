7d5b3a34d45c17e012d79370d88ccfa27b0fff83
Leandro Adrian da Silva <leandrinsilva22@gmail.com>
2024-11-10 14:34:35 -0300

    #

--- src/utils/urlUtils.js
+++ src/utils/urlUtils.js
@@ -1,13 +1,13 @@
 const getImageName = (image) => (String(image).toLowerCase());
-const IMAGE_PATH = 'src/assets/images';
-const ICON_PATH = 'src/assets/icons';
+const IMAGE_PATH = '/images';
+const ICON_PATH = '/icons';
 
 export const imageUrl = (image, path = null) => {
     if (path) {
         return `${IMAGE_PATH}/${path}/${getImageName(image)}`;
     }
 
-    return `src/assets/images/${getImageName(image)}`;
+    return `/${IMAGE_PATH}/${getImageName(image)}`;
 }
 
 export const iconUrl = (image, path = null) => {
