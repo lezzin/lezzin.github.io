2746af20fe4c012c725014a83263b5a5f7809825
Leandro Adrian da Silva <leandrinsilva22@gmail.com>
2024-11-09 22:35:58 -0300

    refactor: add smooth scroll to libs folder

--- src/main.js
+++ src/main.js
@@ -1,26 +1,8 @@
 import { createApp } from 'vue';
+import raf from './libs/smoothscroll';
 import App from './App.vue';
-import Lenis from 'lenis';
 
 import './assets/style.css';
-import 'lenis/dist/lenis.css'
 
 createApp(App).mount('#app');
-
-const lenis = new Lenis({
-    duration: 1.2,
-    direction: 'vertical',
-    gestureDirection: 'vertical',
-    smooth: true,
-    mouseMultiplier: 1,
-    smoothTouch: false,
-    touchMultiplier: 2,
-    infinite: false,
-})
-
-function raf(time) {
-    lenis.raf(time)
-    requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
+requestAnimationFrame(raf);
