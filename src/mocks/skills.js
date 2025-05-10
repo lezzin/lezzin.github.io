0ab3709f60e643e1320d3d1bccea65a9f9ea9621
leandrin <leandrinsilva22@gmail.com>
2025-05-10 12:37:22 -0300

    add new skills

--- src/mocks/skills.js
+++ src/mocks/skills.js
@@ -4,11 +4,16 @@ const skills = [
     { name: "JavaScript", description: "Linguagem de programação para adicionar interatividade e lógica à web." },
     { name: "PHP", description: "Linguagem de script do lado do servidor para desenvolvimento de aplicações web dinâmicas." },
     { name: "CodeIgniter", description: "Framework PHP leve para construção de aplicações web rápidas e eficientes." },
+    { name: "Laravel", description: "Framework PHP moderno e robusto para desenvolvimento de aplicações web escaláveis." },
     { name: "Vuejs", description: "Framework JavaScript progressivo para construção de interfaces de usuário." },
     { name: "Quasar", description: "Framework baseado em Vue.js para criação de aplicações multiplataforma." },
     { name: "jQuery", description: "Biblioteca JavaScript para simplificar a manipulação do DOM e chamadas AJAX." },
     { name: "SASS", description: "Extensão de CSS que adiciona funcionalidades como variáveis e mixins." },
-    { name: "Bootstrap", description: "Framework CSS para desenvolvimento rápido e responsivo de interfaces web." }
+    { name: "Bootstrap", description: "Framework CSS para desenvolvimento rápido e responsivo de interfaces web." },
+    {
+        name: "TypeScript",
+        description: "Superset do JavaScript que adiciona tipagem estática para melhorar a escalabilidade e a manutenção do código.",
+    },
 ];
 
 export default skills;
